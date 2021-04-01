abe02b2d50c23c1226ce3a3f583ef2561be5b4a5
hellbrandt <hellbrandt@googlemail.com>
2021-04-01 13:54:28 +0100

    Add Gulp, setup for Less and weapon sheet tweaks

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// Less configuration
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+gulp.task('less', function (cb) {
+  gulp
+    .src('less/wh3e.less')
+    .pipe(less())
+    .pipe(
+      gulp.dest('css/')
+    );
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('less', function (cb) {
+    gulp.watch('less/*.less', gulp.series('less'));
+    cb();
+  })
+);
--- module/sheets/WH3ItemSheet.js
+++ module/sheets/WH3ItemSheet.js
@@ -1,4 +1,13 @@
 export default class WH3ItemSheet extends ItemSheet {
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      width: 530,
+      height: 400,
+      classes: ["wh3e", "sheet", "item"]
+    })
+  }
+
   get template() {
     return `systems/wh3e/templates/sheets/${this.item.data.type}-sheet.html`;
   }
