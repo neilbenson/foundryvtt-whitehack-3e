c8b2f957de4ba0c81ebb088c4c9c9e8245d89dde
hellbrandt <hellbrandt@googlemail.com>
2021-04-07 19:11:32 +0100

    Add mod input for physical stats

--- wh3e.js
+++ wh3e.js
@@ -29,9 +29,14 @@ Hooks.once("init", function () {
 
   preloadHandlebarsTemplates();
 
-  Handlebars.registerHelper("getTextFromKey", function (group, key) {
+  Handlebars.registerHelper("getTextFromKey", (group, key) => {
     const languageKey = group + "." + key;
     const languageValue = game.i18n.localize(languageKey);
     return new Handlebars.SafeString(languageValue);
   });
+
+  Handlebars.registerHelper("showModifier", (key) => {
+    const showArrayFor = ['str', 'dex', 'con'];
+    return showArrayFor.find(element => element === key);
+  })
 })
