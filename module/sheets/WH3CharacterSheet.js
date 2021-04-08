6935390c5a152bea01e83659a8a9e722da5be5b2
hellbrandt <hellbrandt@googlemail.com>
2021-04-08 15:18:08 +0100

    Enable add, edit and delete of gear and weapons

--- module/sheets/WH3CharacterSheet.js
+++ module/sheets/WH3CharacterSheet.js
@@ -15,18 +15,7 @@ export default class WH3CharacterSheet extends ActorSheet {
     data.config = CONFIG.wh3e;
     data.weapons = data.items.filter((item) => item.type === "Weapon");
     data.gear = data.items.filter((item) => item.type === "Gear");
-    data.abilities = data.items.filter((item) => {
-      if (item.type === "Ability") {
-        let cleanedDescription = item.data.description.replace(/(<([^>]+)>)/ig, '');
-        let descriptionLength = cleanedDescription.length;
-        item.data.description = cleanedDescription.replace(/(<([^>]+)>)/ig, '')
-          .slice(0, 24);
-        if (descriptionLength > 24) {
-          item.data.description = item.data.description.concat('...');
-        }
-        return item;
-      }
-    });
+    data.abilities = data.items.filter((item) => item.type === "Ability");
 
     return data;
   }
@@ -44,13 +33,28 @@ export default class WH3CharacterSheet extends ActorSheet {
     let element = event.currentTarget;
 
     let itemData = {
-      name: game.i18n.localize("wh3e.sheet.newAbility"),
+      name: game.i18n.localize("wh3e.sheet.new" + element.dataset.type),
       type: element.dataset.type,
       data: {
-        type: "slot",
         description: ""
       }
     };
+
+    if (element.dataset.type === "Ability") {
+      itemData.data.type = "slot";
+    }
+
+    if (element.dataset.type === "Gear") {
+      itemData.img = "icons/svg/mystery-man.svg";
+      itemData.data.weight = "regular";
+    }
+
+    if (element.dataset.type === "Weapon") {
+      itemData.img = "icons/svg/mystery-man.svg";
+      itemData.data.damage = 'd6';
+      itemData.data.weight = "regular";
+      itemData.data.rateOfFire = "none";
+    }
 
     return this.actor.createOwnedItem(itemData);
   }
